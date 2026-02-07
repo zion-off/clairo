503af456236b5f7c7e6c1ff76a4dc62e1d0d80d2
zion <zion@nyu.edu>
2026-02-07 23:53:54 +0600

    remove tabs

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,10 @@
 import { Box } from 'ink';
 import GitHubView from './components/github/GitHubView';
-import { TabItem, Tabs } from './components/ui/Tabs';
 
 export default function App() {
   return (
     <Box flexGrow={1} flexDirection="column" overflow="hidden">
-      <Tabs>
-        <TabItem name="GitHub">
-          <GitHubView />
-        </TabItem>
-      </Tabs>
+      <GitHubView />
     </Box>
   );
 }
